54593d7fe3f084739be22b4c1b9715142b3320f1
RaenonX <raenonx0710@gmail.com>
2020-09-23 17:41:03 -0500

    ADD - precommit shell script
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

---
+++ precommit.sh
@@ -0,0 +1,30 @@
+# ------ Variables
+
+CLR_RED=[31m
+CLR_GRN=[32m
+CLR_CYN=[36m
+CLR_NC=[0m
+
+# ------ Functions
+
+run_cmd_exit_on_err() {
+  if ! $1; then
+    echo "${CLR_RED}Error @ $2${CLR_NC}"
+    read -p "Press enter to continue." -r
+    exit 1
+  fi
+}
+
+# ------ Main
+
+echo "${CLR_CYN}Checking with pylint...${CLR_NC}"
+run_cmd_exit_on_err "pylint msnmetrosim" "pylint check"
+
+echo "${CLR_CYN}Checking with pydocstyle...${CLR_NC}"
+run_cmd_exit_on_err "pydocstyle msnmetrosim --count" "pydocstyle check"
+
+echo "${CLR_CYN}Running code tests...${CLR_NC}"
+run_cmd_exit_on_err pytest "code test"
+
+echo "--- ${CLR_GRN}All checks passed.${CLR_NC} ---"
+read -p "Press enter to continue." -r
