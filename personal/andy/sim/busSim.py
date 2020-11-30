3d9c618337970fa82825cf9df38617db02e22fc2
Andy Lin <lin383@wisc.edu>
2020-11-30 17:13:47 +0800

    ADD - ipynb file for generating points based on population density

---
+++ personal/andy/sim/busSim.py
@@ -0,0 +1,143 @@
+import pandas as pd
+import geopandas as gpd
+from datetime import datetime
+from graph import Graph
+import os
+
+
+class BusSim:
+
+    def __init__(
+        self,
+        data_path,
+        day,
+        start_time,
+        elapse_time,
+        avg_walking_speed,
+        max_walking_min,
+        route_remove=[],
+        trip_delays=[]
+    ):
+        """The constructor of the BusSim class
+
+        Args:
+            data_path (str): The path to the directory of the data files
+                (contains both mmt_gtfs and plot subdirectories)
+
+            day (str): the day in a week to perform simulation on
+
+            start_time (str): the starting time to perform simulation on
+                (HH:MM:SS)
+
+            elapse_time (str): the elapse time from starting time to perform
+                simulation on (HH:MM:SS)
+
+            avg_walking_speed (float): the assumed average walking speed
+
+            max_walking_min (float): the maximum allowed walking time minutes
+
+            route_remove (:obj:`list` of :obj:`int`, optional): the list
+                of routes to remove
+
+            trip_delays (:obj:`list` of :obj:`Tuple`, optional): the list
+                of trip-delay pairs to add the tuple should be in the format of
+                `(trip_id, delay in HH:MM:SS)`
+
+        """
+        self.data_path = data_path
+        self.day = day
+        self.start_time = start_time
+        self.elapse_time = elapse_time
+        self.avg_walking_speed = avg_walking_speed
+        self.max_walking_min = max_walking_min
+        self.max_walking_distance = max_walking_min * 60.0 * avg_walking_speed
+        self.stopTimes_final_df = self._gen_final_df(route_remove, trip_delays)
+        self.graph = Graph(self.stopTimes_final_df, start_time,
+                           elapse_time, self.max_walking_distance, avg_walking_speed)
+
+    def get_gdf(self, start_stop):
+        gdf = self.graph.get_gdf(start_stop)
+
+        gdf['geometry_centriod'] = gdf.geometry
+
+        # https://epsg.io/3174
+        gdf = gdf.to_crs(epsg=3174)
+        gdf['geometry'] = gdf.geometry.buffer(gdf['radius'])
+        gdf = gdf.to_crs(epsg=4326)
+        return gdf
+
+    def get_area(self, gdf):
+        # the area returned is in meters^2
+        lake_path = os.path.join(
+            self.data_path, "plot", "background", "water-shp")
+        lakes = gpd.read_file(lake_path)
+        lakes = lakes.to_crs(epsg=3174)
+        gdf = gdf.to_crs(epsg=3174)
+        return gdf.unary_union.difference(lakes.unary_union).area
+
+    def _gen_final_df(self, route_remove, trip_delays):
+        mmt_gtfs_path = os.path.join(self.data_path, "mmt_gtfs")
+        stops_df = pd.read_csv(os.path.join(
+            mmt_gtfs_path, "stops.csv"), sep=",")
+        trips_df = pd.read_csv(os.path.join(
+            mmt_gtfs_path, "trips.csv"), sep=",")
+        stopTimes_df = pd.read_csv(os.path.join(
+            mmt_gtfs_path, "stop_times.csv"), sep=",")
+        calendar_df = pd.read_csv(os.path.join(
+            mmt_gtfs_path, "calendar.csv"), sep=",")
+
+        # get valid service_ids
+        calendar_df['start_date'] = pd.to_datetime(
+            calendar_df['start_date'], format='%Y%m%d')
+        calendar_df['end_date'] = pd.to_datetime(
+            calendar_df['end_date'], format='%Y%m%d')
+        calendar_filtered_df = calendar_df[self._is_service_valid(
+            calendar_df[self.day], calendar_df["start_date"], calendar_df["end_date"])]
+        service_ids = calendar_filtered_df["service_id"].tolist()
+
+        # get valid trips
+        trips_df = trips_df[trips_df["service_id"].isin(service_ids)]
+
+        # remove routes
+        trips_filtered_df = trips_df[~trips_df["route_short_name"].isin(
+            route_remove)]
+
+        # get valid stop_times
+        stopTimes_filtered_df = trips_filtered_df.merge(
+            stopTimes_df, on="trip_id")
+        stopTimes_merged_df = stopTimes_filtered_df.merge(stops_df, on="stop_id")[
+            ["service_id", "route_short_name", "trip_id", "stop_id", "stop_sequence", "arrival_time", "shape_dist_traveled", "stop_lat", "stop_lon", "cardinal_direction"]]
+
+        # get stop_times within the time frame
+        stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('24')), 'arrival_time'] = \
+            stopTimes_merged_df.arrival_time.str.replace('24', '00')
+        stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('25')), 'arrival_time'] = \
+            stopTimes_merged_df.arrival_time.str.replace('25', '01')
+        stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('26')), 'arrival_time'] = \
+            stopTimes_merged_df.arrival_time.str.replace('26', '02')
+        stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('27')), 'arrival_time'] = \
+            stopTimes_merged_df.arrival_time.str.replace('27', '03')
+        stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('28')), 'arrival_time'] = \
+            stopTimes_merged_df.arrival_time.str.replace('28', '04')
+        stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('29')), 'arrival_time'] = \
+            stopTimes_merged_df.arrival_time.str.replace('29', '05')
+        stopTimes_merged_df['arrival_time'] = pd.to_timedelta(
+            stopTimes_merged_df['arrival_time'])
+
+        # add trip_delays
+        for (trip_id, delay) in trip_delays:
+            stopTimes_merged_df.loc[stopTimes_merged_df["trip_id"]
+                                    == trip_id, "arrival_time"] += pd.to_timedelta(delay)
+
+        stopTimes_final_df = self._get_valid_stopTime(
+            stopTimes_merged_df, self.start_time, self.elapse_time).sort_values(by="arrival_time")
+
+        return stopTimes_final_df
+
+    def _is_service_valid(self, day, start_date, end_date):
+        return (day == 1) & (datetime.now() >= start_date) & (datetime.now() <= end_date)
+
+    def _get_valid_stopTime(self, df, start_time, elapse_time):
+        start_time = pd.to_timedelta(start_time)
+        end_time = start_time + pd.to_timedelta(elapse_time)
+        return df[(df['arrival_time'] > start_time) & (df['arrival_time'] < end_time)]
---
+++ personal/andy/sim/graph.py
@@ -0,0 +1,198 @@
+from datetime import timedelta
+from math import sin, cos, asin, sqrt, pi, ceil, floor
+from collections import defaultdict
+import heapq
+import pandas as pd
+import geopandas as gpd
+
+
+class Node:
+    def __init__(self, trip_id, stop_sequence, stop_id, stop_lat, stop_lon, arrival_time, max_walking_distance):
+        self.trip_id = trip_id
+        self.stop_sequence = stop_sequence
+        self.stop_id = stop_id
+        self.stop_lat = stop_lat
+        self.stop_lon = stop_lon
+        self.arrival_time = arrival_time
+        # this should be modified by search in graph
+        self.walking_distance = max_walking_distance
+        self.children = []
+
+    def distance(self, other):
+        """Calculates the distance between two points on earth using the
+        harversine distance (distance between points on a sphere)
+        See: https://en.wikipedia.org/wiki/Haversine_formula
+
+        :return: distance in meters between points
+        """
+
+        lat1, lon1, lat2, lon2 = (
+            a/180*pi for a in [self.stop_lat, self.stop_lon, other.stop_lat, other.stop_lon])
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = sin(dlat/2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon/2) ** 2
+        c = 2 * asin(min(1, sqrt(a)))
+        d = 3956 * 1609.344 * c
+        return d
+
+    def __str__(self):
+        return f"trip_id: {self.trip_id} stop_sequence: {self.stop_sequence} stop_id: {self.stop_id} stop_lat: {self.stop_lat} stop_lon: {self.stop_lon} arrival_time: {self.arrival_time} walking_distance: {self.walking_distance}"
+
+    def __repr__(self):
+        rv = self.__str__()
+        rv += "\nChildren:\n"
+        for child in self.children:
+            rv += f"  cost: {child.cost} "
+            child = child.node
+            rv += str(child)
+            rv += "\n"
+        return rv
+
+    def __lt__(self, other):
+        # always retain sequence here
+        return False
+
+
+class NodeCostPair:
+    def __init__(self, node, cost):
+        self.node = node
+        self.cost = cost  # the cost here means the walking distance
+
+
+class Graph:
+    def __init__(self, df, start_time, elapse_time, max_walking_distance, avg_walking_speed):
+        self.df = df
+        self.start_time = start_time
+        self.elapse_time = elapse_time
+        self.max_walking_distance = max_walking_distance
+        self.avg_walking_speed = avg_walking_speed
+        self.nodes = []
+        self._constuct_graph(max_walking_distance)
+
+    def get_gdf(self, start_stop):
+        start = self._find_start(start_stop)
+        self._clear_graph()
+        self._dijkstra(start)
+
+        rows = dict()
+        start_time = pd.to_timedelta(self.start_time)
+        end_time = start_time + pd.to_timedelta(self.elapse_time)
+        for node in self.nodes:
+            if node.walking_distance < self.max_walking_distance:
+                radius = self.max_walking_distance - node.walking_distance
+                time_left = (end_time - node.arrival_time).total_seconds()
+                radius = min(radius, self.avg_walking_speed * time_left)
+                if node.stop_id not in rows or radius > rows[node.stop_id][3]:
+                    rows[node.stop_id] = [node.stop_id, node.stop_lon,
+                                          node.stop_lat, radius]
+        rows = [row for row in rows.values()]
+
+        df = pd.DataFrame(
+            rows, columns=['stop_id', 'stop_lon', 'stop_lat', 'radius'])
+        gdf = gpd.GeoDataFrame(
+            df, geometry=gpd.points_from_xy(df.stop_lon, df.stop_lat), crs="EPSG:4326")
+
+        return gdf
+
+    def _clear_graph(self):
+        for node in self.nodes:
+            node.walking_distance = self.max_walking_distance
+
+    def _dijkstra(self, start):
+        start.walking_distance = 0
+        pq = [(0, start)]
+        while len(pq) > 0:
+            curr_distance, curr_node = heapq.heappop(pq)
+
+            if curr_distance > curr_node.walking_distance:
+                continue
+
+            for child in curr_node.children:
+                cost = child.cost
+                child = child.node
+
+                distance = curr_distance + cost
+
+                if distance < child.walking_distance:
+                    child.walking_distance = distance
+                    heapq.heappush(pq, (distance, child))
+
+    def _constuct_graph(self, max_walking_distance):
+        # gen nodes
+        trip_node_dict = defaultdict(list)
+        stop_node_dict = defaultdict(list)
+
+        map_grid = []
+        lat_stepsize = max_walking_distance / 11.1 * 0.0001
+        lon_stepsize = max_walking_distance / 8.1 * 0.0001
+        min_lat = self.df.stop_lat.min()
+        max_lat = self.df.stop_lat.max()
+        min_lon = self.df.stop_lon.min()
+        max_lon = self.df.stop_lon.max()
+
+        lat_num = ceil((max_lat - min_lat) / lat_stepsize)
+        lon_num = ceil((max_lon - min_lon) / lon_stepsize)
+        for i in range(lat_num):
+            lat_list = []
+            for j in range(lon_num):
+                lat_list.append([])
+            map_grid.append(lat_list)
+
+        for index, row in self.df.iterrows():
+            node = Node(row["trip_id"], row["stop_sequence"], row["stop_id"], row["stop_lat"],
+                        row["stop_lon"], row["arrival_time"], self.max_walking_distance)
+            self.nodes.append(node)
+            trip_node_dict[row["trip_id"]].append(node)
+            stop_node_dict[row["stop_id"]].append(node)
+            lat_bucket = floor((row["stop_lat"] - min_lat) / lat_stepsize)
+            lon_bucket = floor((row["stop_lon"] - min_lon) / lon_stepsize)
+            map_grid[lat_bucket][lon_bucket].append(node)
+
+        # gen edges
+        # direct sequence
+        for trip_id, nodes in trip_node_dict.items():
+            for i in range(len(nodes)-1):
+                start = nodes[i]
+                end = nodes[i+1]
+                nodeCostPair = NodeCostPair(end, 0)
+                start.children.append(nodeCostPair)
+
+        # wait on the stop
+        for stop_id, nodes in stop_node_dict.items():
+            for i in range(len(nodes)):
+                for j in range(i+1, len(nodes)):
+                    start = nodes[i]
+                    end = nodes[j]
+                    nodeCostPair = NodeCostPair(end, 0)
+                    start.children.append(nodeCostPair)
+
+        # walk
+        for lat in range(lat_num):
+            for lon in range(lon_num):
+                start_bucket = map_grid[lat][lon]
+                end_buckets = []
+                for lat_end in range(max(0, lat-1), min(lat_num, lat+2)):
+                    for lon_end in range(max(0, lon-1), min(lon_num, lon+2)):
+                        end_buckets.append(map_grid[lat_end][lon_end])
+
+                for start in start_bucket:
+                    for end_bucket in end_buckets:
+                        for end in end_bucket:
+                            if start.arrival_time >= end.arrival_time:
+                                continue
+
+                            # walk
+                            distance = start.distance(end)
+                            time_delta = distance / self.avg_walking_speed
+                            time_delta = timedelta(seconds=time_delta)
+                            if distance < self.max_walking_distance and start.arrival_time + time_delta < end.arrival_time:
+                                nodeCostPair = NodeCostPair(end, distance)
+                                start.children.append(nodeCostPair)
+
+    def _find_start(self, start_stop):
+        start = None
+        for node in self.nodes:
+            if node.stop_id == start_stop:
+                if start is None or node.arrival_time < start.arrival_time:
+                    start = node
+        return start
