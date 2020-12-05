fcf34f68777177d68f95b8e6cb93553132766c79
xcwisc <cxu249@wisc.edu>
2020-12-04 20:02:59 -0600

    update test

--- Chang/test.py
+++ Chang/test.py
@@ -7,62 +7,62 @@ import time
 import requests
 from datetime import datetime, timedelta
 from shapely.geometry import Point
+from busSim import BusSim
 from graph import Graph
 
+
+DATA_PATH = "/Users/changxu/Desktop/projects/Madison-Metro-Sim/data"
 DAY = "monday"
-START_STOP = 765
-START_TIME = "10:00:00"
+START_TIME = "12:00:00"
 ELAPSE_TIME = "00:10:00"
-AVG_WALKING_SPEED = 3
+AVG_WALKING_SPEED = 1.4  # 1.4 meters per second
 MAX_WALKING_MIN = 10
-MAX_WALKING_DISTANCE = MAX_WALKING_MIN / 60.0 * AVG_WALKING_SPEED
-
-stops_df = pd.read_csv('data/chang/stops.txt', sep=",")
-trips_df = pd.read_csv('data/chang/trips.txt', sep=",")
-routeID_df = pd.read_csv('data/chang/routes.txt', sep=",")
-stopTimes_df = pd.read_csv('data/chang/stop_times.txt', sep=",")
-shape_df = pd.read_csv('data/chang/shapes.txt')
-calendar_df = pd.read_csv('data/chang/calendar.txt')
-
-def valid_service(day, start_date, end_date):
-    return (day == 1) & (datetime.now() >= start_date) & (datetime.now() <= end_date)
 
 
-def get_valid_stopTime(df, start_time, elapse_time):
-    start_time = pd.to_timedelta(start_time)
-    end_time = start_time + pd.to_timedelta(elapse_time)
-    return df[(df['arrival_time'] > start_time) & (df['arrival_time'] < end_time)]
+def read_stop_ids():
+    ids = []
 
-# get valid service_ids
-calendar_df['start_date'] = pd.to_datetime(calendar_df['start_date'], format='%Y%m%d')
-calendar_df['end_date'] = pd.to_datetime(calendar_df['end_date'], format='%Y%m%d')
-calendar_filtered_df = calendar_df[valid_service(calendar_df[DAY], calendar_df["start_date"], calendar_df["end_date"])]
-service_ids = calendar_filtered_df["service_id"].tolist()
+    with open('stop_ids.txt', 'r') as f:
+        for line in f:
+            stop_id = int(line[:-1])
+            ids.append(stop_id)
 
-# get valid trips
-trips_filtered_df = trips_df[trips_df["service_id"].isin(service_ids)]
+    return ids
 
-# get valid stop_times
-stopTimes_filtered_df = trips_filtered_df.merge(stopTimes_df, on="trip_id")
 
-stopTimes_merged_df = stopTimes_filtered_df.merge(stops_df, on="stop_id")[["service_id", "route_short_name", "trip_id", "stop_id", "stop_sequence", "arrival_time", "shape_dist_traveled", "stop_lat", "stop_lon", "cardinal_direction"]]
+def gen_expected():
+    result = {}
+    stop_ids = read_stop_ids()
+    busSim = BusSim(DATA_PATH, DAY, START_TIME, ELAPSE_TIME,
+                    AVG_WALKING_SPEED, MAX_WALKING_MIN)
+    c = 0
+    for stop_id in stop_ids:
+        gdf = busSim.get_gdf(stop_id)
+        result[stop_id] = busSim.get_area(gdf)
+        c += 1
+        print(c)
 
-# get stop_times within the time frame
-stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('24')),'arrival_time'] = \
-    stopTimes_merged_df.arrival_time.str.replace('24', '00')
-stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('25')),'arrival_time'] = \
-    stopTimes_merged_df.arrival_time.str.replace('25', '01')
-stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('26')),'arrival_time'] = \
-    stopTimes_merged_df.arrival_time.str.replace('26', '02')
-stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('27')),'arrival_time'] = \
-    stopTimes_merged_df.arrival_time.str.replace('27', '03')
-stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('28')),'arrival_time'] = \
-    stopTimes_merged_df.arrival_time.str.replace('28', '04')
-stopTimes_merged_df.loc[(stopTimes_merged_df.arrival_time.str.startswith('29')),'arrival_time'] = \
-    stopTimes_merged_df.arrival_time.str.replace('29', '05')
-stopTimes_merged_df['arrival_time'] = pd.to_timedelta(stopTimes_merged_df['arrival_time'])
+    with open('expected.json', 'w') as f:
+        json.dump(result, f)
 
-stopTimes_final_df = get_valid_stopTime(stopTimes_merged_df, START_TIME, ELAPSE_TIME).sort_values(by="arrival_time")
 
-test_graph = Graph(stopTimes_final_df, START_STOP, MAX_WALKING_DISTANCE, AVG_WALKING_SPEED)
-test_graph.dijkstra()
+def run_test():
+    expected = {}
+    with open('expected.json', 'r') as f:
+        expected = json.load(f)
+
+    stop_ids = read_stop_ids()
+    busSim = BusSim(DATA_PATH, DAY, START_TIME, ELAPSE_TIME,
+                    AVG_WALKING_SPEED, MAX_WALKING_MIN)
+
+    c = 0
+    for stop_id in stop_ids:
+        gdf = busSim.get_gdf(stop_id)
+        assert expected[str(stop_id)] == busSim.get_area(gdf)
+        c += 1
+        print(c)
+
+
+if __name__ == "__main__":
+    # gen_expected()
+    run_test()
